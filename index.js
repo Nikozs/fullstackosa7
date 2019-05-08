406b1ab0721a43da31ffd464fc3c0712fbcfd0c8
Nikozs <niko.purola@gmail.com>
2019-05-08 22:13:48 +0300

    -

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ mongoose.Promise = global.Promise
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true")
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 next();
